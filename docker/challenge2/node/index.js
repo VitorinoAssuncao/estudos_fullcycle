bc8723b0bbfe39b9e377a5483b64affe2ea2264c
Vitorino M Assuncao <vitorinomuller@gmail.com>
2024-01-18 21:13:27 -0300

    feat: add node js files

---
+++ docker/challenge2/node/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: "nodedb"
+};
+
+const connection = mysql.createConnection(config)
+
+connection.query(`INSERT INTO people(name) values ('Vitorino');`)
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+
+app.get('/', (req, res) => {
+    const query = 'SELECT name FROM people';
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error executing query: ' + error.stack);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      const names = results.map((result) => result.name);
+      res.render('index', { names });
+    });
+  });
+  
+
+app.listen(port, () => {
+    console.log('Running in port '+port)
+})
