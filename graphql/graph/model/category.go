684148e7cb9eb349061fb5341908a378c8417106
Vitorino M Assuncao <vitorinomuller@gmail.com>
2024-02-07 17:20:14 -0300

    update files

--- graphql/graph/model/category.go
+++ graphql/graph/model/category.go
@@ -3,6 +3,6 @@ package model
 type Category struct {
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
+	Description *string   `json:"description"`
 	Courses     []*Course `json:"courses"`
 }
