edb11a7791b3ccb685f5060d65e6f377ef9b16fa
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-03-12 15:25:49 -0300

    add unit tests

--- ddd-pattern/src/entity/customers.spec.ts
+++ ddd-pattern/src/entity/customers.spec.ts
@@ -1,7 +1,57 @@
+import Address from "./address";
+import Customer from "./customers";
+
 describe("Customer unit tests", () =>{
-    it("should get 1 as result",() =>{
-      const result = 1;
-      expect(result).toBe(1);  
+  it("should create customer successfully",() =>{
+    const customer = new Customer("abc", "Jhon");
+    expect(customer.name).toBe("Jhon");
+    expect(customer.id).toBe("abc");
+  });
+
+    it("should throw error when id is empty",() =>{
+      expect(() =>{
+        let customer = new Customer("","John")
+      }).toThrow("Id is required")
+    });
+
+    it("should throw error when id is empty",() =>{
+      expect(() =>{
+        let customer = new Customer("abc","")
+      }).toThrow("Name is required")
+    });
+
+    it("should change name",() =>{
+      const customer = new Customer("abc", "Jhon");
+      customer.changeName("Abel");
+
+      expect(customer.name).toBe("Abel")
+    });
+
+    it("should activate customer",() =>{
+      const customer = new Customer("abc", "Jhon");
+      const address = new Address("Rua dos Bobos", 123, "00000","Cidade dos Loucos");
+
+      customer.Address = address;
+
+      customer.activate();
+
+      expect(customer.isActive()).toBe(true)
+    });
+
+    it("should deactivate customer",() =>{
+      const customer = new Customer("abc", "Jhon");
+
+      customer.deactivate();
+
+      expect(customer.isActive()).toBe(false)
+    });
+
+    it("should throw error when address is undefined",() =>{
+      const customer = new Customer("abc", "Jhon");
+
+      expect(() =>{
+        customer.activate()
+      }).toThrow("Address is mandatory to activate a customer")
     });
 
 })
--- ddd-pattern/src/entity/order.ts
+++ ddd-pattern/src/entity/order.ts
@@ -1,15 +1,45 @@
+import { error } from "console";
 import OrderItem from "./order_item";
 
-class Order{
-    _id:string;
-    _customerID:string;
-    _items: OrderItem[] = [];
+export default class Order{
+    private _id:string;
+    private _customerID:string;
+    private _items: OrderItem[] = [];
 
     constructor(id:string, customerID:string, items:OrderItem[]){
         this._id = id;
         this._customerID = customerID;
         this._items = items;
+
+        this.validate()
     }
 
+    get id(): string {
+        return this._id;
+      }
+
+      get customerID(): string {
+        return this._customerID;
+      }
+
+      get items(): OrderItem[] {
+        return this.items;
+      }
+
+      validate() {
+        if (this._id.length === 0) {
+          throw new Error("Id is required");
+        }
+        if (this._customerID.length === 0) {
+          throw new Error("CustomerID is required");
+        }
+        if (this._items.length === 0){
+            throw new Error("Need at least one item per order")
+        } 
+      }
+
+    total(): number{
+        return this._items.reduce((acc, item) => acc + item.price, 0)
+    }
 
 }
