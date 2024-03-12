6667c3da63d19b3743fc3d06bdd6ffd73a1bedbf
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-03-12 13:55:57 -0300

    feat: ddd patern exercise

---
+++ ddd-pattern/src/entity/order.ts
@@ -0,0 +1,15 @@
+import OrderItem from "./order_item";
+
+class Order{
+    _id:string;
+    _customerID:string;
+    _items: OrderItem[] = [];
+
+    constructor(id:string, customerID:string, items:OrderItem[]){
+        this._id = id;
+        this._customerID = customerID;
+        this._items = items;
+    }
+
+
+}
