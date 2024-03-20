497e1d99546e937a1ae24c0f08208f77b8800f4f
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-03-20 15:06:52 -0300

    reorder files

---
+++ ddd-pattern/src/domain/repository/product-interface.ts
@@ -0,0 +1,6 @@
+import RepositoryInterface from "./interfaces";
+import Product from "../entity/product";
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product>{
+    
+}
