4c0e1518342a345000030be113990f79f0623930
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-06-03 10:27:11 -0300

    refactor: add notification to product

--- clean-arch/src/domain/product/entity/product.spec.ts
+++ clean-arch/src/domain/product/entity/product.spec.ts
@@ -11,25 +11,31 @@ describe("Product unit tests", () =>{
     it("should throw error when id is empty",() =>{
         expect(() =>{
         let product = new Product("","Product",1)
-        }).toThrow("Id is required")
+        }).toThrow("product: Id is required")
     });
 
     it("should throw error when name is empty",() =>{
         expect(() =>{
         let product = new Product("123","",1)
-        }).toThrow("Name is required")
+        }).toThrow("product: Name is required")
+    });
+
+    it("should throw error when name and id are empty",() =>{
+        expect(() =>{
+        let product = new Product("","",1)
+        }).toThrow("product: Id is required, product: Name is required")
     });
 
     it("should throw error when price is zero",() =>{
         expect(() =>{
         let product = new Product("123","Product",0)
-        }).toThrow("Price need to be greater than zero")
+        }).toThrow("product: Price need to be greater than zero")
     });
 
     it("should throw error when price is negative",() =>{
         expect(() =>{
         let product = new Product("123","Product",-5)
-        }).toThrow("Price need to be greater than zero")
+        }).toThrow("product: Price need to be greater than zero")
     });
 
     it("should change name",() =>{
--- clean-arch/src/domain/product/entity/product.ts
+++ clean-arch/src/domain/product/entity/product.ts
@@ -1,13 +1,16 @@
 import { error } from "console";
 import ProductInterface from "./product.interface";
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 
-export default class Product implements ProductInterface{
-    private _id:string;
+export default class Product extends Entity{
+    protected _id:string;
     private _name:string;
     private _price:number;
 
 
     constructor(id:string,name:string, price:number){
+        super();
         this._id = id;
         this._name = name;
         this._price = price;
@@ -30,14 +33,29 @@ export default class Product implements ProductInterface{
 
       validate() {
         if (this._id.length === 0) {
-          throw new Error("Id is required");
+          this.notification.addError({
+            message: "Id is required",
+            context: "product",
+          });
         }
+        
         if (this._name.length === 0) {
-          throw new Error("Name is required");
+          this.notification.addError({
+            message: "Name is required",
+            context: "product",
+          });
         }
+
         if (this._price <= 0){
-            throw new Error("Price need to be greater than zero")
+          this.notification.addError({
+            message: "Price need to be greater than zero",
+            context: "product",
+          });
         } 
+
+        if (this.notification.hasErrors()) {
+          throw new NotificationError(this.notification.errors);
+        }
       }
 
       changeName(newName:string){
