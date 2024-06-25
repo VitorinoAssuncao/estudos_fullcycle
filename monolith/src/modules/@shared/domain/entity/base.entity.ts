ef53a8a9339f29cf8803eaa21e74a80dcf6dc261
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-06-25 13:52:23 -0300

    feat: shared structure

---
+++ monolith/src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,29 @@
+import ID from "../vo/id.vo";
+
+export default class BaseEntity {
+    private _id: ID;
+    private _createdAt: Date;
+    private _updatedAt: Date;
+
+    constructor(id?: ID, createdAt?: Date, updatedAt?: Date){
+        this._id = id || new ID();
+        this._createdAt = createdAt || new Date();
+        this._updatedAt = updatedAt || new Date();
+    }
+
+    get id(): ID{
+        return this._id;
+    }
+
+    get createdAt(): Date{
+        return this._createdAt;
+    }
+
+    get updatedAt(): Date{
+        return this._updatedAt;
+    }
+
+    set updatedAt(date: Date){
+        this._updatedAt = date;
+    };
+}
