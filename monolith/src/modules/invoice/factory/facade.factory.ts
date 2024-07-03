27dd9018ed62c5cb95158470414866c67d78a2e8
Vitorino Assuncao <vitorino.muller@gmail.com>
2024-07-03 17:58:29 -0300

    test: fixed

--- monolith/src/modules/invoice/factory/facade.factory.ts
+++ monolith/src/modules/invoice/factory/facade.factory.ts
@@ -1,13 +1,13 @@
-import FindByIDUsecase from "../../store-catalog/usecase/find-by-id/find-by-id.usecase";
 import InvoiceFacade from "../facade/invoice.facade";
 import InvoiceRepository from "../repository/invoice.repository";
+import FindInvoiceUsecase from "../usecase/find-invoice.usecase";
 import GenerateInvoiceUseCase from "../usecase/generate-invoice.usecase";
 
 export default class InvoiceFacadeFactory {
     static create() {
         const repository = new InvoiceRepository();
         const generateInvoiceUseCase = new GenerateInvoiceUseCase(repository);
-        const findInvoiceUseCase = new FindByIDUsecase(repository);
+        const findInvoiceUseCase = new FindInvoiceUsecase(repository);
 
         return new InvoiceFacade({
             generateInvoiceUseCase: generateInvoiceUseCase,
